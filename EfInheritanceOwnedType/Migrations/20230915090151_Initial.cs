e28b98031db57419c74da71dde2bf46c3d48696f
JoasE <32096708+JoasE@users.noreply.github.com>
2023-09-15 11:07:28 +0200

    Initial commit

---
+++ EfInheritanceOwnedType/Migrations/20230915090151_Initial.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfInheritanceOwnedType.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+migrationBuilder.CreateTable(
+    name: "SubTypeOnes",
+    columns: table => new
+    {
+        Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+    },
+    constraints: table =>
+    {
+        table.PrimaryKey("PK_SubTypeOnes", x => x.Id);
+    });
+
+migrationBuilder.CreateTable(
+    name: "SubTypeTwos",
+    columns: table => new
+    {
+        Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+    },
+    constraints: table =>
+    {
+        table.PrimaryKey("PK_SubTypeTwos", x => x.Id);
+    });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubTypeOnes");
+
+            migrationBuilder.DropTable(
+                name: "SubTypeTwos");
+        }
+    }
+}
---
+++ EfInheritanceOwnedType/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+
+
+public abstract class BaseType
+{
+    public BaseType(OwnedType ownedType)
+    {
+        OwnedType = ownedType;
+    }
+
+    protected BaseType(){}
+
+    public Guid Id { get; } = Guid.NewGuid();
+    public OwnedType OwnedType { get; }
+}
+
+public class SubTypeOne : BaseType
+{
+    public SubTypeOne(OwnedType ownedType) : base(ownedType) {}
+    private SubTypeOne() {}
+}
+
+public class SubTypeTwo : BaseType
+{
+    public SubTypeTwo(OwnedType ownedType) : base(ownedType) {}
+    private SubTypeTwo() {}
+}
+
+
+public class OwnedType
+{
+    public OwnedType(Guid id)
+    {
+        Id = id;
+    }
+    public Guid Id { get; }
+}
+
+
+
+public class TestDbContext : DbContext
+{
+    public TestDbContext() : base(new DbContextOptionsBuilder<TestDbContext>().UseSqlServer().Options) {}
+
+    public DbSet<SubTypeOne> SubTypeOnes { get; set; }
+
+    public DbSet<SubTypeTwo> SubTypeTwos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+
+modelBuilder.Owned<OwnedType>();
+modelBuilder.Entity<BaseType>(baseType =>
+{
+    baseType.UseTpcMappingStrategy();
+
+    baseType.Property(x => x.Id);
+    baseType.OwnsOne(x => x.OwnedType, cfg => cfg.Property(x => x.Id));
+});
+    }
+}
